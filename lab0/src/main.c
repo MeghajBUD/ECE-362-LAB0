cb178a80105aaade1c7414fb7d81b587f2876de6
MeghajKK <kabrameg000@hsestudents.org>
2024-08-22 22:23:56 -0400

    Step 5 Complete

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -1,11 +1,13 @@
 int main(int argc, char const *argv[])
 {
     /* code */
-    int x, y;
-    for (int i = 0; i < 10; x++){}
-    if (x == 10) {
-        y++;
-        x=0;
+    int x=0, y=0;
+    for (;;){
+        x++;
+        if (x == 10) {
+            y++;
+            x=0;
+        }
     }
     
     return 0;
